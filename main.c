637a899a27d1325a22df0a79fdf95576f4573093
artrayme <artrayme@protonmail.com>
2021-11-16 19:11:33 +0300

    feat: memory initializer

--- main.c
+++ main.c
@@ -21,9 +21,27 @@ int block_size;
 int blocks_count;
 int memory_size;
 
+int init_memory(int _block_size, int _blocks_count) {
+  if (_block_size < 1 || _blocks_count < 1) return -1;
+  memory_size = _block_size * _blocks_count;
+  blocks_count = _blocks_count;
+  block_size = _block_size;
+
+  main_ptr.block = malloc(memory_size);
+  ptr max_address = main_ptr;
+  max_address.block+=memory_size;
+  for (ptr allocation_pointer = main_ptr; allocation_pointer.block < max_address.block; allocation_pointer.block += _block_size) {
+    allocation_pointer.block->header = 0b00001101;
+  }
+  current_block = main_ptr;
+  return memory_size;
+}
+
+
+
 int main() {
 
-//  init_memory(64, 10);
+  init_memory(64, 10);
 
   return 0;
 }
